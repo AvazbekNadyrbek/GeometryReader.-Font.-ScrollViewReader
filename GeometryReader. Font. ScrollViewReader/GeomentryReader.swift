97b994d7a7d3cdbb2907a896ef9aef6169e59949
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-08-31 10:38:45 +0200

    Here i am learned a GeometryReader, and how we can use it, its so simply uisng proxy.

---
+++ GeometryReader. Font. ScrollViewReader/GeomentryReader.swift
@@ -0,0 +1,18 @@
+//
+//  GeomentryReader.swift
+//  GeometryReader. Font. ScrollViewReader
+//
+//  Created by Авазбек Надырбек уулу on 31.08.25.
+//
+
+import SwiftUI
+
+struct GeomentryReader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeomentryReader()
+}
