3f9b0dcdcd64c4b6bc7f3452ec323d3ab396344b
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-08-31 08:45:05 +0200

    Initial Commit

---
+++ GeometryReader. Font. ScrollViewReader/GeometryReader__Font__ScrollViewReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReader__Font__ScrollViewReaderApp.swift
+//  GeometryReader. Font. ScrollViewReader
+//
+//  Created by Авазбек Надырбек уулу on 31.08.25.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReader__Font__ScrollViewReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
